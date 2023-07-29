43c1e84dbaffe2a2eb2d1eed28a7b328a0655fdc
naomiHauret <hauretnaomi@gmail.com>
2023-07-29 16:14:12 +0200

    feat: add authentication hook using arweavekit and othent

---
+++ apps/pwa/src/features/authentication/hooks/use-authentication.tsx
@@ -0,0 +1,146 @@
+import { createContext, useContext, onMount } from 'solid-js'
+import { createStore } from 'solid-js/store'
+import { createMutation, type CreateMutationResult } from '@tanstack/solid-query'
+import { Othent } from 'arweavekit/auth'
+import type { UserDetailsReturnProps } from 'arweavekit/dist/types/auth'
+
+interface Authentication {
+  /** Wheter the wallet is connected */
+  connected: boolean
+  /** wallet address associated with a user's email account */
+  walletAddress: string
+  /** */
+  provider: typeof Othent
+  profile:
+    | {
+        firstName: string
+        nickname: string
+        fullname: string
+        picture: string
+        locale: string
+        email: string
+        emailVerificationStatus: string
+      }
+    | undefined
+}
+
+/**
+ * SolidJS context for managing on-chain authentication/online identity state and operations
+ */
+const AuthenticationContext = createContext<{
+  /** SolidJS store that contains data related to the on-chain authentication/online identity state and operations */
+  storeAuthentication: Authentication
+
+  /** Sign the current user in */
+  mutationSignIn: CreateMutationResult<UserDetailsReturnProps, unknown, void, unknown>
+
+  /** Mutate current user info */
+  mutationCurrentUser: CreateMutationResult<UserDetailsReturnProps, unknown, void, unknown>
+
+  /** Sign the current user out */
+  mutationSignOut: CreateMutationResult<unknown, unknown, void, unknown>
+}>()
+
+/**
+ * On-chain identity store default state
+ */
+const DEFAULT_STATE: Authentication = {
+  provider: Othent,
+  connected: false,
+  walletAddress: undefined,
+  profile: undefined,
+}
+
+export const OTHENT_PARAMETERS = {
+  apiId: import.meta.env.VITE_OTHENT_APP_ID,
+}
+
+export function AuthenticationProvider(props) {
+  const [storeAuthentication, setStoreAuthentication] = createStore(DEFAULT_STATE)
+
+  // Refresh session on mount
+  onMount(async () => {
+    await mutationCurrentUser.mutateAsync()
+  })
+
+  const mutationCurrentUser = createMutation({
+    mutationFn: async (): Promise<UserDetailsReturnProps> => {
+      return await Othent.userDetails(OTHENT_PARAMETERS)
+    },
+    onSuccess(data) {
+      if (data) {
+        setStoreAuthentication({
+          connected: true,
+          walletAddress: data?.contract_id,
+          profile: {
+            firstName: data?.given_name,
+            nickname: data?.nickname,
+            fullname: data?.name,
+            picture: data?.picture,
+            locale: data?.locale,
+            email: data?.email,
+            emailVerificationStatus: data?.email_verified,
+          },
+        })
+      }
+    },
+  })
+
+  const mutationSignIn = createMutation({
+    mutationFn: async () => {
+      return await Othent.logIn(OTHENT_PARAMETERS)
+    },
+    onSuccess(data) {
+      if (data) {
+        setStoreAuthentication({
+          connected: true,
+          walletAddress: data?.contract_id,
+          profile: {
+            firstName: data?.given_name,
+            nickname: data?.nickname,
+            fullname: data?.name,
+            picture: data?.picture,
+            locale: data?.locale,
+            email: data?.email,
+            emailVerificationStatus: data?.email_verified,
+          },
+        })
+      }
+    },
+  })
+
+  const mutationSignOut = createMutation({
+    mutationFn: async (): Promise<void> => {
+      return await Othent.logOut(OTHENT_PARAMETERS)
+    },
+    onMutate() {
+      setStoreAuthentication(DEFAULT_STATE)
+    },
+  })
+
+  /**
+   * Query for retrieving current user loyalty information from the smart contract
+   */
+
+  return (
+    <AuthenticationContext.Provider
+      value={{
+        storeAuthentication,
+        mutationSignIn,
+        mutationCurrentUser,
+        mutationSignOut,
+      }}
+    >
+      {props.children}
+    </AuthenticationContext.Provider>
+  )
+}
+
+/**
+ * Hook for accessing the auth context (current user, chain provider, mutations...)
+ *
+ * @returns {AuthenticationContextValue} Authentication context value.
+ */
+export function useAuthentication() {
+  return useContext(AuthenticationContext)
+}
---
+++ apps/pwa/src/screens/Landing.tsx
@@ -0,0 +1,15 @@
+export const Landing = () => {
+  return (
+    <>
+      <main class="flex-grow h-full bg-complementary-2">
+        <p class="font-bold text-9xl text-primary-8">Gommette</p>
+        <p class="font-bold text-9xl text-primary-9">Gommette</p>
+        <p class="font-bold text-9xl text-primary-10">Gommette</p>
+        <p class="font-bold text-9xl text-primary-11">Gommette</p>
+        <p class="font-bold text-9xl text-primary-12">Gommette.</p>
+      </main>
+    </>
+  )
+}
+
+export default Landing
